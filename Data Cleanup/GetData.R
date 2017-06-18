062219890839670c4f67e4eeae303a70200ed007
Chiranjeevi Mallavarapu <chiranjeevi.mallavarapu@gmail.com>
2017-06-18 10:38:40 -0700

    First part of the code

---
+++ Data Cleanup/GetData.R
@@ -0,0 +1,11 @@
+# Authors: MSSMU_May2017 : Chiranjeevi Mallavarapu, Dave Dyer, Kim Wong, Maryam Shahini 
+# Taken from Doing Data Science Assignment Week 6 by Dr. Mahesh Fernando
+
+library(plyr)
+
+setwd("/Data/")
+
+# http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page
+
+# read csv file
+bk <- read.csv("rollingsales_brooklyn.csv",skip=4,header=TRUE)
