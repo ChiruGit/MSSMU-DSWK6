2483dd8a9bf8b3b6f13aa5b53669c05bb8898eb3
Chiran Mallavarapu <cmallavarapu@Chirans-iMac.local>
2017-06-21 11:00:36 -0700

    comments updated

--- DataCleanup/GetData.R
+++ DataCleanup/GetData.R
@@ -1,17 +1,17 @@
 # Authors: MSSMU_May2017 : Chiranjeevi Mallavarapu, Dave Dyer, Kim Wong, Maryam Shahini 
 # Taken from Doing Data Science Assignment Week 6 by Dr. Mahesh Fernando, TAs Sahil and Raunak
 
-## Start by <Dave Dyer> : Data Gathering
+## START BY <DAVE DYER> : DATA GATHERING
 library(plyr)
 
 # http://www1.nyc.gov/site/finance/taxes/property-rolling-sales-data.page
 
-# Set root path of the project on your local computer -<this is the only change you need to do 
-setwd("/Users/cmallavarapu/Documents/Chiran/Git/MSSMU-DSWK6")
+# Sets root path of the project on your local computer 
+# setwd("/Users/cmallavarapu/Documents/Chiran/Git/MSSMU-DSWK6")
 
-# Program then goes to the Data folder to read csv file
+# Program then goes to the relative path of the Data folder to read csv file
 si <- read.csv("DataCleanup/Data/rollingsales_statenisland.csv",skip=4,header=TRUE)
 
 # bk <- read.csv("rollingsales_brooklyn.csv",skip=4,header=TRUE)
 
-## End by <Dave Dyer> : Data Gathering
+## END BY <DAVE DYER> : DATA GATHERING
